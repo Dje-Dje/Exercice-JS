94c5cad8ef4a84630885e45bed15dc9b5ab3b3d5
Dje <andre.jerome@protonmail.com>
2017-11-01 22:53:52 +0100

    Ajout des elseif/else, fin de la boucle

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,3 +3,15 @@
 for (var nombre = 1; nombre <= 100; nombre++) {
     if ((nombre % 3 === 0) && (nombre % 5 === 0)) {
         console.log("FizzBuzz");
+    }else if ((nombre % 3 === 0) && (nombre % 7 === 0)){
+        console.log("FizzWoof");
+    }
+    }else if (nombre % 3 === 0){
+        console.log("Fizz");
+    }else if (nombre % 5 === 0){
+        console.log("Buzz");
+    } else if (nombre % 7 === 0){
+        console.log("Woof");
+    } else {
+        console.log(nombre);}
+}
